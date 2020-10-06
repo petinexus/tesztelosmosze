4adb9e389a344dc0b3cbe119a096d729fcce4ddc
petinexus <petinexus@gmail.com>
2020-10-06 12:28:37 +0200

    update

--- osztaly.h
+++ osztaly.h
@@ -1,7 +1,21 @@
-
-
-/*! \class asd
-* \brief majd kiderül
+/**
+ * \class ezt irja a classnal
+ * 
+ * \ingroup ezt irja a ingroupnal
+ * 
+ * \brief ez itt a brief neke a része
+ * 
+ * This is a longer description....This is a longer description....This is a longer description....This is a longer description....This is a longer description....
+ * 
+ * \note ez itt a note neke a része
+ * 
+ * \author ez vagyok én Voznek Péter
+ * 
+ * \version alpha 0.1
+ * 
+ * \date 2020/10/06
+ * 
+ * Created on: 2020/10/03 22:07
 */
 
 class osztaly
