9dab0d9a44038e74a3cee91a9b4b007abc302972
petinexus <petinexus@gmail.com>
2020-10-07 17:12:36 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "osztaly.h"
 
+
+/**
+ * \class ASD
+ *  
+ * \brief ez itt az ASD osztaly
+ * 
+ * This is decription....
+ * 
+ * \note ez itt a note neke a része
+*/
+
 int main()
 {
     std::cout << "Hello world!" << std::endl;
